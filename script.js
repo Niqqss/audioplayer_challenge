e3da46c727d029fd1d7ac586b280cbf59c24980a
Nicolas Stefanelly <nstefanelly@gmail.com>
2023-04-01 02:11:52 +0200

    debut responsive + interface + son

--- script.js
+++ script.js
@@ -13,19 +13,44 @@ const context = canvas.getContext('2d');
 let width = canvas.width;
 let height = canvas.height;
 
-const resizeCanvas = () => {
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-    width = canvas.width;
-    height = canvas.height;
-};
-
 let audio;
 let audioContext, audioData, sourceNode, analyserNode;
+let minDb, maxDb;
+
+const numCircles = 5;
+const numSlices = 1;
+const slice = Math.PI * 2 / numSlices;
+const radius = 200;
+
+const bins = [];
+const lineWidths = [];
+const rotationOffsets = [];
+
+let lineWidth, bin, mapped, phi;
+
+let prevTime = 0;
+let angle = 0;
+const angularVelocity = 0.00004;
+
+for (let i = 0; i < numCircles * numSlices; i++) {
+    bin = Math.floor(Math.random() * 61) + 4;
+    bins.push(bin)
+}
+
+for (let i = 0; i < numCircles; i++) {
+    const t = i / (numCircles - 1);
+    lineWidth = customQuadIn(t) * 200 + 10;
+    lineWidths.push(lineWidth);
+}
+
+for (let i = 0; i < numCircles; i++) {
+    const min = Math.PI * -0.25;
+    const max = Math.PI * 0.25;
+    const rand = Math.random() * (max - min) + min;
+    rotationOffsets.push(rand - Math.PI * 0.5);
+}
 
 const sketch = () => {
-    const bins = [4, 12, 37, 165];
-
     return ({ context, width, height }) => {
         context.clearRect(0, 0, width, height);
         context.fillStyle = 'transparent';
@@ -35,32 +60,54 @@ const sketch = () => {
 
         analyserNode.getFloatFrequencyData(audioData);
 
-        for (let i = 0; i < bins.length; i++) {
-            const bin = bins[i];
-            const mapped = mapRange(audioData[bin], analyserNode.minDecibels, analyserNode.maxDecibels, 0, 1, true);
-            const radius = mapped * 230;
+        const currentTime = performance.now();
+        const deltaTime = currentTime - prevTime;
+        prevTime = currentTime;
 
+        angle += angularVelocity * deltaTime;
+
+        context.save();
+        context.translate(width * 0.5, height * 0.5);
+        context.rotate(angle);
+        context.scale(1, -1);
+
+        let cradius = radius;
+
+        for (let i = 0; i < numCircles; i++) {
             context.save();
-            context.translate(width * 0.5, height * 0.5);
+            context.rotate(rotationOffsets[i]);
 
-            // Draw multiple copies of the lines with reduced opacity and a slight offset
-            for (let j = 0; j < 5; j++) {
-                const alpha = (j + 1) / 5 ;
-                const offset = j - 2;
+            cradius += lineWidths[i] * 0.5 + 2;
+
+            for (let j = 0; j < numSlices; j++) {
+                context.rotate(slice);
+                context.lineWidth = lineWidths[i];
+
+                bin = bins[i * numSlices + j];
+
+                mapped = mapRange(audioData[bin], minDb, maxDb, 0, 1, true);
+                lineWidth = lineWidths[i] * mapped;
+
+                phi = slice * mapped;
+
                 context.beginPath();
-                context.arc(offset, offset, radius, 0, Math.PI * 2);
-                context.strokeStyle = `rgba(25, 25, 25, ${alpha})`;
-                context.lineWidth = 10;
+                context.lineWidth = lineWidth;
+                context.arc(0, 0, cradius, 0, phi);
                 context.stroke();
             }
 
+            cradius += lineWidths[i] * 0.5;
+
             context.restore();
         }
+
+        context.restore();
     };
 };
 
 const initializeAudioAnalyzer = () => {
     audio = document.querySelector('audio');
+    audio.volume = volumeControl.value / 100;
 
     audioContext = new AudioContext();
 
@@ -72,31 +119,19 @@ const initializeAudioAnalyzer = () => {
     analyserNode.smoothingTimeConstant = 0.95;
     sourceNode.connect(analyserNode);
 
+    minDb = analyserNode.minDecibels;
+    maxDb = analyserNode.maxDecibels;
+
     audioData = new Float32Array(analyserNode.frequencyBinCount);
 }
 
-const getAverage = (data) => {
-    let sum = 0;
-
-    for (let i = 0; i < data.length; i++) {
-        sum += data[i];
-    }
-
-    return sum / data.length;
+function mapRange(value, inputMin, inputMax, outputMin, outputMax, clamp) {
+    const mapped = (value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin;
+    return clamp ? Math.min(outputMax, Math.max(outputMin, mapped)) : mapped;
 }
 
-function mapRange(value, low1, high1, low2, high2, clamped) {
-    const val = low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-    if (clamped) {
-        if (low2 < high2) {
-            if (val < low2) return low2;
-            else if (val > high2) return high2;
-        } else {
-            if (val > low2) return low2;
-            else if (val < high2) return high2;
-        }
-    }
-    return val;
+function customQuadIn(t) {
+    return t * t;
 }
 
 const start = async () => {
@@ -107,8 +142,6 @@ const start = async () => {
     animate();
 }
 
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
 start();
 
 // FIN CANVAS // FIN CANVAS // FIN CANVAS // FIN CANVAS // FIN CANVAS // FIN CANVAS // FIN CANVAS // FIN CANVAS
@@ -256,12 +289,6 @@ function playAShuffledSong() {
 // CODE PERSO A REPLACER
 
 const playlist = document.querySelector('.playlist');
-const switchContentBtn = document.querySelector('.switch-content-btn');
-
-switchContentBtn.addEventListener("click", () => {
-    playlist.classList.toggle("visible");
-    thumbnail.classList.toggle("hidden");
-})
 
 musicsData.forEach(musicData => {
     const playlistItem = document.createElement("li");
@@ -288,7 +315,7 @@ playlistItems.forEach((playlistItem, index) => {
             currentlyPlayingItem.classList.remove("currently-playing");
         }
         const [title, artist] = playlistItem.textContent.split(" - ");
-        const itemData = { title, artist};
+        const itemData = { title, artist };
         populateUI(itemData);
         if (!audioContext) initializeAudioAnalyzer();
         play();
@@ -298,3 +325,10 @@ playlistItems.forEach((playlistItem, index) => {
         }
     });
 });
+
+const volumeControl = document.querySelector(".volume-control");
+volumeControl.addEventListener("mousemove", function (e) {
+    if (audio) {
+        audio.volume = e.currentTarget.value / 100;
+    }
+})
